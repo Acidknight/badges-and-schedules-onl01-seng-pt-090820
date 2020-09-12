232119e74fd0a1d24c1bcb4589c981731ec52c94
Anthony Leon <acidknights89@gmail.com>
2020-09-12 05:23:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,5 +20,5 @@ end
 
 def printer(attendees)
   puts batch_badge_creator(attendees).each{|badge| puts badge}
-  puts assign_rooms(attendees).each{|
+  puts assign_rooms(attendees).each{|room| puts room}
 end
