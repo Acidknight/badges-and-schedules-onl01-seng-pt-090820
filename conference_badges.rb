ebb1d5734b7968a926cb7c3c133a8e4d4a2ac9e8
Anthony Leon <acidknights89@gmail.com>
2020-09-12 04:38:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,6 @@
 
 def badge_maker(name)
-  return {
+  return "Hello, my name is #{name}"
 
 def batch_badge_creator
 conference_speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
