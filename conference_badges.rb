1a40be261da079056004bdf58523489eada55bf2
Anthony Leon <acidknights89@gmail.com>
2020-09-12 05:05:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,11 +11,7 @@ def batch_badge_creator(array)
 end
 
 def assign_rooms(speaker)
-  room = 0 
-  speaker.collect do |name|
-    room += 1 
-    "Hello, #{name}! You'll be assigned to room #{room}!"
-  end
+  
 end
 
 def printer
